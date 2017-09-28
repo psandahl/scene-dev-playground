d07a2f9e5793db82142a48e0ad4953526f0a219d
Patrik Sandahl <patrik.sandahl@ericsson.com>
2017-09-28 16:43:18 +0200

    Testing dummy program loading.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+--import           Control.Concurrent.Async
 import           Graphics.Scene
 
 main :: IO ()
@@ -8,4 +9,11 @@ main =
         createViewer "Dev Playground" defaultConfiguration [ ClearColor 1 0 0 0 ]
 
 runViewer :: Viewer -> IO ()
-runViewer = waitOnViewerClose
+runViewer viewer = do
+    res <- loadShaderProgram viewer $
+        ProgramRequest
+            { shaders = [(Vertex, "foo"), (Fragment, "bar")]
+            , uniforms = []
+            }
+    print res
+    waitOnViewerClose viewer
