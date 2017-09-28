b5893af715a19d4c5cd88558d9f50dbabb913474
Patrik Sandahl <patrik.sandahl@ericsson.com>
2017-09-28 13:29:27 +0200

    Small refactoring.

--- src/Main.hs
+++ src/Main.hs
@@ -3,8 +3,9 @@ module Main where
 import           Graphics.Scene
 
 main :: IO ()
-main = do
-    eViewver <- createViewer "Dev Playground" defaultConfiguration [ ClearColor 1 0 0 0 ]
-    case eViewver of
-        Right viewer -> waitOnViewerClose viewer
-        Left err     -> putStrLn err
+main =
+    either putStrLn runViewer =<<
+        createViewer "Dev Playground" defaultConfiguration [ ClearColor 1 0 0 0 ]
+
+runViewer :: Viewer -> IO ()
+runViewer = waitOnViewerClose
