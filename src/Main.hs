128319b5bb5d567ab375c51bfef8442b04f91258
Patrik Sandahl <kosmoskatten.software@gmail.com>
2017-10-07 17:38:33 +0200

    New Scene impl. Simplest example.

--- src/Main.hs
+++ src/Main.hs
@@ -1,55 +1,23 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
---import           Control.Concurrent.Async
-import           Control.Monad        (void)
-import           Data.Vector.Storable (Vector, fromList)
-import           Graphics.GL          (GLfloat, GLuint)
-import           Graphics.Scene
-import           Linear               (V3 (..))
+import           Scene
 
 main :: IO ()
-main = void $ viewScenes defaultConfiguration viewApp
+main = do
+    res <- viewScenes defaultConfiguration onInit onEvent onExit
+    print res
 
-viewApp :: Viewer -> IO ()
-viewApp viewer = do
-    shaderRes <- loadShaderProgram viewer $
-        ProgramRequest
-            { shaders = [ (Vertex, "resources/vertex.glsl")
-                        , (Fragment, "resources/fragment.glsl")
-                        ]
-            , uniformNames = ["col"]
-            }
+onInit :: Viewer -> IO ()
+onInit _viewer = putStrLn "onInit"
 
-    meshRes <- loadMesh viewer $
-        MeshRequest
-            { vertices = triangleVertices
-            , primitive = Triangles
-            , indices = triangleIndices
-            }
+onEvent :: Viewer -> Event -> () -> IO ()
 
-    case shaderRes of
-        Right shader ->
+onEvent viewer CloseRequest _ = do
+    putStrLn "Close now!"
+    close viewer
 
-            case meshRes of
-                Right mesh' -> do
-                    let entity = Entity [] shader [UniformValue "col" triangleColor] mesh'
-                    setScene viewer $ Scene [Clear [ColorBufferBit, DepthBufferBit]] [entity]
+--onEvent _viewer event _ =
+--    putStrLn $ "onEvent: " ++ show event
 
-                Left err -> logStrLn viewer err
-
-        Left err -> logStrLn viewer err
-
-triangleVertices :: Vector VertexWithPos
-triangleVertices =
-    fromList
-        [ VertexWithPos { position = V3 0 1 0}
-        , VertexWithPos { position = V3 (-1) (-1) 0}
-        , VertexWithPos { position = V3 1 (-1) 0}
-        ]
-
-triangleIndices :: Vector GLuint
-triangleIndices = fromList [0, 1, 2]
-
-triangleColor :: V3 GLfloat
-triangleColor = V3 1 1 0
+onExit :: Viewer -> () -> IO ()
+onExit _viewer _ = putStrLn "onExit"
