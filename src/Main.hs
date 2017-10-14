16e34b888cfe123e2b9d6d32d3c44b29ee24b97c
Patrik Sandahl <kosmoskatten.software@gmail.com>
2017-10-14 23:10:45 +0200

    With a static, untransformed, triangle.

--- src/Main.hs
+++ src/Main.hs
@@ -1,34 +1,62 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           Data.Vector.Storable             (Vector, fromList)
+import           Graphics.GL                      (GLfloat, GLuint)
+import           Linear                           (V3 (..))
 import           Scene
+import           Scene.GL.Attribute.VertexWithPos (VertexWithPos (..))
 --import           Text.Printf (printf)
 
 main :: IO ()
 main = do
-    let config = defaultConfiguration { globalActions =
-                                            [ClearColor 1 0 0 1]
-                                      , initialScene =
-                                          Scene { actions =
-                                                    [ Clear [ColorBufferBit]
-                                                    , Enable DepthTest
-                                                    ]
-                                                }
-                                      }
+    let config = defaultConfiguration
+            { globalActions =
+                [ClearColor 1 0 0 1]
+            , initialScene =
+                Scene
+                    { sceneActions =
+                        [ Clear [ColorBufferBit]
+                        ]
+                    , sceneEntities = []
+                    }
+            }
     res <- viewScenes config onInit onEvent onExit
     print res
 
 onInit :: Viewer -> IO ()
 onInit viewer = do
     putStrLn "onInit - enter"
-    result <- programFromFiles viewer
+    progResult <- programFromFiles viewer
         ProgramRequest
             { shaders = [ (Vertex, "resources/vertex.glsl")
                         , (Fragment, "resources/fragment.glsl")
                         ]
             , uniformNames = ["col"]
             }
-    print result
+
+    case progResult of
+        Right program -> do
+            meshResult <- meshFromRequest viewer
+                MeshRequest
+                    { vertices = triangleVertices
+                    , indices = triangleIndices
+                    , primitive = Triangles
+                    }
+            case meshResult of
+                Right mesh -> do
+                    let entity = Screen { screenActions = []
+                                        , screenProgram = program
+                                        , screenMesh = mesh
+                                        , screenUniforms = [ UniformValue "col" triangleColor ]
+                                        }
+                    setCurrentScene viewer
+                        Scene { sceneActions = [Clear [ColorBufferBit]]
+                              , sceneEntities = [entity]
+                              }
+                Left err -> putStrLn err
+        Left err      -> putStrLn err
+
     putStrLn "onInit - done"
 
 onEvent :: Viewer -> Event -> () -> IO ()
@@ -42,3 +70,16 @@ onEvent _viewer _ _ = return ()
 
 onExit :: Viewer -> () -> IO ()
 onExit _viewer _ = putStrLn "onExit"
+
+triangleVertices :: Vector VertexWithPos
+triangleVertices = fromList
+    [ VertexWithPos { position = V3 0 1 0 }
+    , VertexWithPos { position = V3 (-1) (-1) 0 }
+    , VertexWithPos { position = V3 1 (-1) 0 }
+    ]
+
+triangleIndices :: Vector GLuint
+triangleIndices = fromList [0, 1, 2]
+
+triangleColor :: V3 GLfloat
+triangleColor = V3 0 0 1
